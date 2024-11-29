3d9c4a4394c97c89616c14a2b9ed1cf673465768
Chalisapat <chalisa.pat@student.mahidol.ac.th>
2024-11-29 22:28:40 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
 int main(){
-    int n,sum =0;
+    int n;
 
     scanf("%d", &n);
 
-    int arr1[n], arr2[n];
-    
-    for (int i = 0; i < n; i++){
-        scanf("%d", &arr1[i]);
-    }
-    
-    for (int i = 0; i < n; i++){
-        scanf("%d", &arr2[i]);
-    }
+    int arr[n];
 
     for (int i = 0; i < n; i++){
-        sum += (arr1[i] * arr2[i]); 
+        scanf("%d", &arr[i]);
     }
 
-    printf("%d", sum);
+    int max = arr[0];
+    int min = arr[1];
+
+    for (int i = 0; i < n; i++){
+        if (i%2==0){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            else{
+                if (arr[i]<min)
+                {
+                    min=arr[i];
+                }
+                
+            }
+        }
+    }
+    
+
+    printf("%d %d", min, max);
 }
