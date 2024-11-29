f8e151a532dddc65993f627ab1272a3e10f05150
Chalisapat <chalisa.pat@student.mahidol.ac.th>
2024-11-29 22:27:27 +0700

    Add files via upload
    example of works

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int n,sum =0;
+
+    scanf("%d", &n);
+
+    int arr1[n], arr2[n];
+    
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr1[i]);
+    }
+    
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr2[i]);
+    }
+
+    for (int i = 0; i < n; i++){
+        sum += (arr1[i] * arr2[i]); 
+    }
+
+    printf("%d", sum);
+}
